11e0d08a3705715cf3315e23e8344705011bdd1a
malarvizhi <malarvizhi.p8248@gmail.com>
2026-07-17 22:17:35 +0530

    day 12

--- frontend/src/pages/ReviewHistory.jsx
+++ frontend/src/pages/ReviewHistory.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 function ReviewHistory() {
   const [reviews, setReviews] = useState([]);
-
+const [search, setSearch] = useState("");
   useEffect(() => {
     fetch("http://localhost:5000/api/reviews")
       .then((response) => response.json())
@@ -16,13 +16,25 @@ function ReviewHistory() {
       <h1 className="text-3xl font-bold mb-6">
         Review History
       </h1>
-
+<input
+  type="text"
+  placeholder="Search reviews..."
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+  className="w-full p-3 mb-6 border rounded-lg"
+/>
       {reviews.length === 0 ? (
         <p className="text-gray-600">
           No reviews found
         </p>
+        
       ) : (
-        reviews.map((review) => (
+
+       reviews
+  .filter((review) =>
+    review.id.toString().includes(search)
+  )
+  .map((review) => (
           <div
             key={review.id}
             className="bg-white p-5 rounded-lg shadow mb-4"
